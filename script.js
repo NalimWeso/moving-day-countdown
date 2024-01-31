f9409d649d67b71eadf8acdfa393fd8b1ea070ff
Milan Wesołowski <155390722+NalimWeso@users.noreply.github.com>
2024-01-31 23:27:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,32 @@ const currentDate = dayjs();
 
 const movingDay = dayjs('2024-09-01');
 
-let dayNumber;
-
 if (currentDate.isBefore(movingDay)) {
+    changeOne = 'will occur';
+    changeTwo = 'will be';
+    changeThree = 'move';
+    changeFour = 'will open';
+    changeFive = 'am eagerly anticipating this';
 
-    dayNumber = `days left: ${movingDay.diff(currentDate, 'day')}`;
+    dayNumber = movingDay.diff(currentDate, 'day');
+    messageContent = `Days left: ${dayNumber}`;
 } else {
+    changeOne = 'occurred';
+    changeTwo = 'was';
+    changeThree = 'moved';
+    changeFour = 'opened';
+    changeFive = 'eagerly anticipated that';
 
-    dayNumber = `days after it was done: ${currentDate.diff(movingDay, 'day')}`;
+    dayNumber = currentDate.diff(movingDay, 'day');
+    messageContent = `Days after it was done: ${dayNumber}`;
 }
 
 document.querySelector('.content').innerHTML = `
-${dayNumber}
-`
+<h1>Moving Day Counter</h1>
+    <p>
+        On September 1, 2024, a significant moment ${changeOne} in my life. It ${changeTwo} the day I ${changeThree} out from my parents'
+        home. This ${changeFour} a new chapter in my life, and since I enjoy changes and challenges, I ${changeFive} date.
+    </p>
+    <hr>
+${messageContent}.
+`;
